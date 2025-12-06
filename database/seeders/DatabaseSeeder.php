f05a1bdfc5d4210a0166556a4d7cea83f7abd00b
Mark <masterofpi11436@gmail.com>
2025-12-05 23:17:19 -0500

    Updated layouts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,5 +26,32 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
             'password' => Hash::make('asd'),
         ]);
+
+        User::factory()->create([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'role' => 'Controller',
+            'theme' => 'dark',
+            'email' => 'test1@example.com',
+            'password' => Hash::make('asd'),
+        ]);
+
+        User::factory()->create([
+            'first_name' => 'Jane',
+            'last_name' => 'Dow',
+            'role' => 'Technician',
+            'theme' => 'dark',
+            'email' => 'test2@example.com',
+            'password' => Hash::make('asd'),
+        ]);
+
+        User::factory()->create([
+            'first_name' => 'Johne',
+            'last_name' => 'Smith',
+            'role' => 'User',
+            'theme' => 'dark',
+            'email' => 'test3@example.com',
+            'password' => Hash::make('asd'),
+        ]);
     }
 }
