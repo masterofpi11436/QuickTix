4d28b0a1096422f3a4a6ce21537a84924b5d3c2d
Mark Tuggle <masterofpi11436@gmail.com>
2025-12-09 13:48:01 -0500

    Fixed theme toggle

--- resources/views/layouts/reporting-user-app.blade.php
+++ resources/views/layouts/reporting-user-app.blade.php
@@ -1,22 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+      class="{{ auth()->check() && auth()->user()->theme === 'dark' ? 'dark' : '' }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'QuickTix') }}</title>
-
-        <script>
-            if (
-                localStorage.theme === 'dark' ||
-                (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-            ) {
-                document.documentElement.classList.add('dark');
-            } else {
-                document.documentElement.classList.remove('dark');
-            }
-        </script>
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
