105bca0f646f3346222b764779f93d4efa56f791
Mark Tuggle <masterofpi11436@gmail.com>
2025-12-23 08:19:28 -0500

    Added a back button for admin pages

--- resources/views/admin/areas/index.blade.php
+++ resources/views/admin/areas/index.blade.php
@@ -1,8 +1,14 @@
 <x-admin-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Areas') }}
-        </h2>
+        <div class="flex items-center justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Areas') }}
+            </h2>
+
+            <x-custom-button href="{{ route('admin.administration') }}" color="green">
+                Administration
+            </x-custom-button>
+        </div>
     </x-slot>
 
     @livewire('admin.areas.search')
