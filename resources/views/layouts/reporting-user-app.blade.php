611807db3e0bb8bf9c4c850b9426887a88c59cc7
Mark Tuggle <masterofpi11436@gmail.com>
2026-01-21 14:07:58 -0500

    Added favicon

--- resources/views/layouts/reporting-user-app.blade.php
+++ resources/views/layouts/reporting-user-app.blade.php
@@ -6,6 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <link rel="icon" href="{{ asset('favicon.svg') }}">
+
         <title>{{ config('app.name', 'QuickTix') }}</title>
 
         <!-- Scripts -->
